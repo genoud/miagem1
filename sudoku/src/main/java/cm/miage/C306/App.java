6668ec0ed0413adeece6d29275e8a0f905b02203
DOUANLA DJATIo Magloire <magloiredjatio@gmail.com>
2014-12-13 03:20:02 +0100

    Squelette projet

---
+++ sudoku/src/main/java/cm/miage/C306/App.java
@@ -0,0 +1,13 @@
+package cm.miage.C306;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
